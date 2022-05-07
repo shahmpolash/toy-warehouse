2e83bf6b71323ffe73946ab522fc0b4bfdb8a95f
Yshahmpolash <iam@shahmpolash>
2022-05-07 16:32:37 +0600

    deliver-button

--- src/components/ToyDetails/ToyDetails.js
+++ src/components/ToyDetails/ToyDetails.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
 import './ToyDetails.css';
 
 const ToyDetails = () => {
+    const addQuantityRef = useRef('');
     const {toydetailId} = useParams();
     const [toy, setToy] =  useState({});
     useEffect( () =>{
@@ -12,11 +13,47 @@ const ToyDetails = () => {
         .then(res=>res.json())
         .then(data=>setToy(data));
     },[]);
-    const {_id, name, price, seller, details, quantity, img } = toy;
+    let {_id, name, price, seller, details, quantity, img } = toy;
     const navigate =  useNavigate()
     const handleUpdate = id =>{
         navigate(`/updateitem/${id}`);
 
+    };
+
+    const handleDeliver = id => {
+        quantity =  quantity - 1;
+        const updateToy = {name, details, price, quantity, seller, img};
+        const resultData = {_id, name, price, seller, details, quantity, img }
+        const url = `http://localhost:5000/toy/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateToy)
+        })
+        .then(res=>res.json())
+        .then(result=>setToy(resultData));
+
+    };
+
+    const handleAddQuantity = event =>{
+        event.preventDefault();
+        const updateId =  _id;
+        quantity =  addQuantityRef.current.value * 1 + quantity * 1;
+        const updateToy = {name, details, price, quantity, seller, img};
+        const resultData = {_id, name, price, seller, details, quantity, img }
+        const url = `http://localhost:5000/toy/${updateId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateToy)
+        })
+        .then(res=>res.json())
+        .then(result=>setToy(resultData));
+        
     }
     
 
@@ -27,13 +64,13 @@ const ToyDetails = () => {
             <h4>Price: ${toy.price}</h4>
             <h4>Quantity: {toy.quantity}</h4>
             <h3 className='p-5'>{toy.details}</h3>
-            <Button>Deliver Now</Button>
+            <Button onClick={() => handleDeliver(_id)} >Deliver Now</Button>
             <br />
             or
             <br />
             
-            <form>
-                <input type="number" name="" id="" />
+            <form onSubmit={handleAddQuantity}>
+                <input type="number" ref={addQuantityRef} name="" id="" />
                 <br />
                 <input className='bg-primary text-white' type="submit" value="Add Quantity" />
             </form>
